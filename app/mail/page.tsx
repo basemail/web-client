d026995a63969aec428876ba28a74ea2f2f56c2b
memosys <82053242+memosys@users.noreply.github.com>
2024-06-10 14:20:29 +0530

    save progress

--- app/mail/page.tsx
+++ app/mail/page.tsx
@@ -5,7 +5,7 @@ import {
   PaperPlaneIcon,
   MagnifyingGlassIcon
 } from '@radix-ui/react-icons';
-import { Grid, Box, Text, TextField, Button, Flex } from "@radix-ui/themes";
+import { Grid, Box, Text, TextField, Button, Flex, Card, Avatar } from "@radix-ui/themes";
 import Header from "@/components/layout/header/Header";
 
 const folders = [
@@ -16,12 +16,43 @@ const folders = [
   { label: 'Trash', icon: <Trash /> }
 ];
 
+const mockEmail = {
+  senderEmail: 'hello@basedMail.com',
+  senderAvatar: 'https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop',
+  senderName: 'BaseMail Official',
+  recipientEmail: 'user@example.com',
+  subject: 'Welcome to BaseMail!',
+  timestamp: '2024-06-10T10:00:00Z',
+  content: `**Welcome to BaseMail!**
+
+  Hi [User's First Name],
+
+  We're excited to have you on board. BaseMail is designed to make your email experience smooth and efficient. Here are a few things you can do to get started:
+
+  - **Set Up Your Profile:** Personalize your account to make it uniquely yours.
+  - **Explore Features:** Check out our intuitive features to help you manage your emails better.
+  - **Get Support:** Have questions? Our support team is here to help you 24/7.
+
+  Thank you for choosing BaseMail. We look forward to providing you with the best email service experience.
+
+  Best regards,
+  The BaseMail Team
+`,
+  attachments: [],
+  isRead: false,
+  priority: 'Normal',
+  replyTo: 'support@basedMail.com',
+  cc: [],
+  bcc: []
+}
+
+
 export default function BaseMail() {
   return (
     <>
       <Header />
       <main className="container mx-auto px-8 py-16 h-full">
-        <Grid columns="3" gap="3" width="auto" className="h-full">
+        <Grid columns="4" gap="3" width="auto" className="h-full">
           <Box id="mail-boxes">
           <TextField.Root placeholder="Search folders…">
             <TextField.Slot>
@@ -30,7 +61,7 @@ export default function BaseMail() {
           </TextField.Root>
           <Flex className='flex-col gap-3 m-4 mt-5'>
             {folders.map(({ icon, label }) => (
-              <Button variant='ghost' size='3' color='gray' key={label} >
+              <Button variant='ghost' size='2' color='gray' key={label} >
                 {icon}
                 <Text className='w-full flex justify-start'>
                 {label}
@@ -39,10 +70,32 @@ export default function BaseMail() {
             ))}
             </Flex>
           </Box>
-          <Box id="mails" style={{ gridColumn: 'span 2' }}>
-            <Text>
-              Hello World
-            </Text>
+          <Box id="mails" style={{ gridColumn: 'span 3' }}>
+          <Box className='w-full'>
+            <Flex className='flex-row w-fit'>
+              <Flex gap="3" align="center">
+                <Avatar
+                  size="3"
+                  src={mockEmail.senderAvatar}
+                  radius="full"
+                  fallback={mockEmail.senderName.substring(0,1).toUpperCase()}
+                />
+                <Box>
+                  <Text as="div" size="2" weight="regular">
+                    {mockEmail.senderName || mockEmail.senderEmail}
+                  </Text>
+                </Box>
+                <Box>
+                <Text as="div" size="2" truncate className='max-w-prose text-white font-medium'>
+                  {mockEmail.subject}
+                </Text>
+                <Text as="div" size="2" color="gray" truncate className='max-w-prose'>
+                  {mockEmail.content}
+                </Text>
+                </Box>
+              </Flex>
+            </Flex>
+          </Box>
           </Box>
         </Grid>
       </main>
